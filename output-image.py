98929f34db2eab0b6f4bc885819461e152fe0187
Richard Coffey <richardc@pemberley.lan>
2018-06-30 21:55:59 -0700

    First commit of RayTracingWeekend --richardc

---
+++ output-image.py
@@ -0,0 +1,27 @@
+import sys
+import random
+import array
+import numpy
+import Image
+
+nx = 200
+ny = 100
+maxval = 255
+j = ny + 1
+i = 0
+ppm_header = 'P6 {nx} {ny} {255}\n'
+
+print "P3\n" + str(nx) + " " + str(ny) + "\n255"
+
+image = array.array('B',[0,0,0] * nx * ny)
+
+for j in range(ny,0,-1) :
+    j = j - 1
+    for i in range(0,nx) :
+        r = float(i) / float(nx)
+        g = float(j) / float(ny)
+        b = 0.2
+        ir = int(255.99*r)
+        ig = int(255.99*g)
+        ib = int(255.99*b)       
+        print str(ir) + " " + str(ig) + " " + str(ib)
