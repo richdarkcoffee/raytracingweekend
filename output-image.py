cb9a8b61a19b379490216b52dbee34cc68883e95
Richard Coffey <19356161+richdarkcoffee@users.noreply.github.com>
2018-07-05 22:04:52 -0700

    Added Pillow functionality to output image files in Python 3 with ammended author

--- output-image.py
+++ output-image.py
@@ -2,28 +2,34 @@ import sys
 import random
 import array
 import numpy
-from PIL import Image
+from PIL import Image, ImageDraw
+
 #This didn't work. But then I found this article and it did
 #https://github.com/python-pillow/Pillow/issues/3068
+
+outfile = "firstimage"
 
 nx = 200
 ny = 100
 maxval = 255
-j = ny + 1
+#j = ny + 1
+j = 0
 i = 0
-ppm_header = 'P6 {nx} {ny} {255}\n'
+#ppm_header = f:'P6 {nx} {ny} {255}\n'
 
 print( "P3\n" + str(nx) + " " + str(ny) + "\n255")
 
-image = array.array('B',[0,0,0] * nx * ny)
-
-for j in range(ny,0,-1) :
-    j = j - 1
+img = Image.new('RGBA', (nx, ny), 'white')    
+idraw = ImageDraw.Draw(img)
+#for j in range(ny,0,-1) :
+for j in range(0,ny) :
     for i in range(0,nx) :
         r = float(i) / float(nx)
         g = float(j) / float(ny)
         b = 0.2
         ir = int(255.99*r)
-        ig = int(255.99*g)
+        ig = 255 - int(255.99*g)
         ib = int(255.99*b)       
-        print( str(ir) + " " + str(ig) + " " + str(ib))
+        idraw.point((i,j),fill=(ir,ig,ib,255))
+
+img.save('firstimage.png')
