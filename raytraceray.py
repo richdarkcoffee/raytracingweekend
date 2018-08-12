ecece83b1fd9dfd8663dacec701af84b0335e9aa
Richard Coffey <19356161+richdarkcoffee@users.noreply.github.com>
2018-08-11 21:09:01 -0700

    First pass at ray class

--- raytraceray.py
+++ raytraceray.py
@@ -5,4 +5,13 @@ import math
 import vec3 from raytracevec
 
 class ray:
-    
+    def __init__(self, a, b):
+        A = a
+        B = b
+    def origin(self):
+        return A
+    def direction(self):
+        return B
+    def point_at_parameter(self,t):
+        return A + t*B
+
